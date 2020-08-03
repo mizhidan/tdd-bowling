0f73410056802add3b34f1ce68410edeacb47369
unknown <dj9824@126.com>
2020-08-03 20:50:06 +0800

    [danjie] feat: spare score calculate test

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -4,8 +4,30 @@ public class BowlingGame {
 
     public int calcScore(List<Integer> bowlingHits) {
         int score = 0;
+        int spareFlag = 1;
+        int prevScore = 0;
+        int roundScore = 0;
+        int extraScoreTime = 0;
+        int round = 0;
+        boolean isTenthRound = false;
         for (Integer integer : bowlingHits) {
             score += integer;
+            if (extraScoreTime > 0 && round != 10) {
+                score += integer;
+                --extraScoreTime;
+            }
+            if (spareFlag == 1) {
+                ++spareFlag;
+            } else {
+                spareFlag = 1;
+                roundScore = score - prevScore;
+                prevScore = score;
+                if (roundScore == 10) {
+                    extraScoreTime += 1;
+                }
+                ++round;
+            }
+
         }
         return score;
     }
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -14,4 +14,14 @@ public class BowlingGameTest {
         int score = bowlingGame.calcScore(bowlingHits);
         assertEquals(64, score);
     }
+
+    @Test
+    void should_calc_correct_score_when_only_have_spare() {
+        List<Integer> bowlingHits1 = Arrays.asList(2,3,4,5,3,7,2,4,5,3,6,2,1,2,4,2,5,1,2,2);
+        List<Integer> bowlingHits2 = Arrays.asList(2,3,4,5,3,7,2,4,5,3,6,2,1,2,4,2,5,1,5,5,3);
+        int score1 = bowlingGame.calcScore(bowlingHits1);
+        int score2 = bowlingGame.calcScore(bowlingHits2);
+        assertEquals(67, score1);
+        assertEquals(76, score2);
+    }
 }
